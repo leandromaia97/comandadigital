c6099f4676dbe5026f3127f5c8124a75546c52a7
leandromaia97 <48168679+leandromaia97@users.noreply.github.com>
2019-11-11 11:57:36 -0300

    Personalização de mensagens de validação e atualizações na autenticação

---
+++ resources/lang/pt-BR/auth.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed'   => 'O e-mail ou senha informado não foi encontrado em nossos registros. Por favor, tente novamente.',
+    'throttle' => 'Você realizou muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+];
